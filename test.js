21ca6e90983087c4d045373c53c4d1501ec84a42
m1mollykay <35971982+m1mollykay@users.noreply.github.com>
2020-02-03 21:26:46 -0500

    Updating test.js file
    Creating new If statement.

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-alert ("Hello, World!");
+var youLikeMeat = true;
+
+if (youLikeMeat) {
+    
+    document.write ("you like meat!");
+    
+} else {
+    
+    document.write ("you hate meat!");
+  
+}
