e93090f5b00efbea5a4c61e212162cee9096fcfb
m1mollykay <35971982+m1mollykay@users.noreply.github.com>
2020-02-10 20:51:59 -0500

    Update test.js file to use for loop
    This is a practical example of a For loop that uses the loop to create an array of all the anchor tags in the HTML document.  The loop cycles through the elements of the anchor tags by adding a dynamic class name of "links" that depends on their position in the array, then on the page.

--- test.js
+++ test.js
@@ -1,22 +1,12 @@
-//for loop: continue or break
+//for loop: practical example using this loop
+//creating an array for anchor tags in the document
 
 //standard name for variables is x or j or i
 //index variable; loop condition; incrementer for ending loop
-for ( i = 0; i <= 10; i++)       
+var links = document.getElementByTagName ("a");
+
+for ( i = 0; i < links.length; i++) {       
+  
+    links [i].className="link-" + i;
     
-    if (i === 3 || i === 5) {         
-    
-        continue;
-    }  //skip code block for 3 or 5 to go back up to the code to start with 4 or 6
-
-console.log (i);
-
-    if (i === 7) {
-        
-        break; 
-    }   //break out of loop for good with no loop for 8 or 9
-
-}
-
-console.log ("I have broken out of the loop");
-    
+}   //end loop
