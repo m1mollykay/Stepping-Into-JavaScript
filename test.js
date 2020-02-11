6120a1fa3eb5de27229112f13b0b25abd2a0a5dd
m1mollykay <35971982+m1mollykay@users.noreply.github.com>
2020-02-11 00:49:06 -0500

    Update test.js file
    To make various functions for logical sections of code, in order to call that code.

--- test.js
+++ test.js
@@ -1,12 +1,49 @@
-//for loop: practical example using this loop
-//creating an array for anchor tags in the document
+//functions
 
-//standard name for variables is x or j or i
-//index variable; loop condition; incrementer for ending loop
-var links = document.getElementByTagName ("a");
-
-for ( i = 0; i < links.length; i++) {       
+function getAverage (a,b) {
   
-    links [i].className="link-" + i;
+    var average = (a + b) /2;
+    console.log (average);
+    return average;
+
+getAverage (7, 12);
+
+getAverage ();
+
+var myResult = getAverage (7,8);
+    console.log ("the average is " + myResult);
+}
+
+function alert(string) {
+    alert ("hello");
+}
+
+function getAverage (a,b,c,d,e,f) {
+    var average = (a + b + c + d + e + f) /6;
+    var myResult = getAverage (7,8,9,10,11,12);
+    console.log ("the average is " + myResult);    
+}
+
+function getAverage (a,b,c,d,e) {
+    var average = (27,32,56,62,75) /5;
+    var myResult = getAverage (27,32,56,62,75);
+    console.log ("this average is " + myResult);
     
-}   //end loop
+}
+
+function getDifference (x,y) {
+    var difference = (x - y);
+    var difference = (6256, 1971);
+    var myResult = getDifference (6256 - 1971);
+    console.log ("the difference is " + myResult);
+}
+
+function alert(string1)
+function alert(string2) {
+    var string1 = ("say_Good_Night_Now");
+    var string2 = ("time_For_Sleep_Good_Night");
+    alert string1 + string2;
+    console.log (string1 + string2);
+}
+//end functions
+  
